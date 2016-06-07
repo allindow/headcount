3cdbc656f6d0fd30d54a1277e25f6d22f6ff398f
Tommasina <tommasina1@gmail.com>
2016-06-06 20:30:54 -0600

    pseudocoding

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -1,6 +1,10 @@
+require 'csv'
+
 class EnrollmentRepository
   #An Enrollment instance holds the enrollment data for a single district. We create an instance like this:
 
+###QUESTIONS###
+#1) should we have one module for all four classes?
 
 def initialize
 end
