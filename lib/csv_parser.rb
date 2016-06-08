c6777e9d36eea27a3143a2d7f2908dd37622dd0a
Angela Lindow <angelalindow@gmail.com>
2016-06-08 11:29:40 -0600

    enrollment data parser working

--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -14,44 +14,23 @@ module CSVParser
     CSV.foreach(filepath, headers: true, header_converters: :symbol).map do |row|
       { :name => row[:location]}
     end.uniq
-    #we could have .uniq at the end of the loop, but shouldn't we have multiple districts to reflect the actual data? ditto for enrollment_repo_parser
-    #no, we just want to know at this point what are the districts.
   end
-
-  def sorted_data(file)
-  all_data = file.map do |row|
-    # if row[:location] == nil
-    #   row[:location] = "UNKNOWN"
-    # else
-    #   row[:location]
-    # end
-      { :name => row[:location], :kindergarten_participation => {row[:timeframe].to_i => row[:data].to_f }}
-  end
-  all_data.sort_by! { |key| key[:name] }
-end
 
   def enrollment_repo_parser(file_tree)
     filepath = file_tree.dig(:enrollment, :kindergarten)
     file = CSV.open(filepath, headers: true, header_converters: :symbol)
-    clean = sorted_data(file)
-     enrollment_data = {}
-     num = 0
-     final = []
-     clean.map do |set|
-       require "pry"; binding.pry
-       if clean[num][:name].upcase == clean[num+1][:name].upcase
-      enrollment_data = enrollment_data.deep_merge(clean[num].deep_merge(clean[num+1]))
-      num += 1
-    elsif clean[num][:name].upcase != clean[num+1][:name].upcase
-      final << enrollment_data
-      enrollment_data = {}
-        enrollment_data = enrollment_data.deep_merge(clean[num].deep_merge(clean[num+1]))
-        num += 1
-        # require "pry"; binding.pry
+    all_data = file.map do |row|
+      { name: row[:location], row[:timeframe].to_i => row[:data].to_f}
     end
-    final
-###   this method mosly works but as some point it is returning nil...need to use test_data to narrow down the problem.
-   end
-
+    enrollment_by_year = all_data.group_by do |row|
+      row[:name]
+    end
+    enrollment_data = enrollment_by_year.map do |name, years|
+      year_data = years.reduce({}, :merge)
+      year_data.delete(:name)
+      {:name => name, :kindergarten_participation => year_data }
+    end
+    enrollment_data
   end
+
 end
