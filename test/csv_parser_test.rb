fcd46cbf40a1d4a95e018e5abbaa6aa66bda2201
Angela Lindow <angelalindow@gmail.com>
2016-06-15 12:55:14 -0600

    csv parser tests pass for economic parser

--- test/csv_parser_test.rb
+++ test/csv_parser_test.rb
@@ -9,12 +9,13 @@ require './lib/enrollment_repository'
 require './lib/enrollment'
 require './lib/district_parser'
 require './lib/enrollment_parser'
+require './lib/economic_parser'
 
 
 class CSVParserTest < Minitest::Test
-  include StatewideParser, DistrictParser, EnrollmentParser
+  include StatewideParser, DistrictParser, EnrollmentParser, EconomicParser
 
-  def test_that_load_districts
+  def test_that_district_parser_load_districts
     file_tree = {
       :enrollment => {
         :kindergarten => "./test/test_data/test_kinder_full_day.csv"
@@ -41,7 +42,7 @@ class CSVParserTest < Minitest::Test
     assert_equal output, group_names(input)
   end
 
-  def test_that_loads_enrollment_names
+  def test_that_enrollment_parser_loads_enrollment_names
     file_tree = {
       :enrollment => {
         :kindergarten => "./test/test_data/test_kinder_full_day.csv"
@@ -51,7 +52,7 @@ class CSVParserTest < Minitest::Test
     assert_equal result, enrollment_repo_parser(file_tree)
   end
 
-  def test_that_loads_only_one_instance_of_an_enrollment_district_name
+  def test_enrollment_parser_loads_only_one_instance_of_an_enrollment_district_name
     file_tree = {
       :enrollment => {
         :kindergarten => "./test/test_data/test_kinder_full_day.csv"
@@ -60,7 +61,7 @@ class CSVParserTest < Minitest::Test
     assert_equal 5, enrollment_repo_parser(file_tree).count
   end
 
-  def test_that_can_get_how_many_files_are_in_file_tree
+  def test_enrollment_parser_can_get_how_many_files_are_in_file_tree
     file_tree = {
       :enrollment => {
         :kindergarten => "./test/test_data/test_kinder_full_day.csv",
@@ -71,42 +72,42 @@ class CSVParserTest < Minitest::Test
     assert_equal i, 2
   end
 
-  def test_can_tell_if_new_district
+  def test_statewide_parser_can_tell_if_new_district
     new_hash = {}
     district_record = {:name=>"COLORADO", :third_grade=>{2008=>{:math=>0.697}}}
     district, category, subject, year, data_point = record_data(district_record)
     assert district_new?(new_hash, district)
   end
 
-  def test_can_tell_if_new_subject
+  def test_statewide_parser_can_tell_if_new_subject
     new_hash = {"COLORADO" => {:name=>"COLORADO", :third_grade=>{2008=>{:math=>0.697}}}}
     district_record = {:name=>"COLORADO", :third_grade=>{2008=>{:reading=>0.703}}}
     district, category, subject, year, data_point = record_data(district_record)
     assert subject_new?(new_hash, district, category, year)
   end
 
-  def test_can_tell_if_new_year
+  def test_statewide_parser_can_tell_if_new_year
     new_hash = {"COLORADO" => {:name=>"COLORADO", :third_grade=>{2008=>{:math=>0.697, :reading=>0.703}}}}
     district_record = {:name=>"COLORADO", :third_grade=>{2009=>{:writing=>0.536}}}
     district, category, subject, year, data_point = record_data(district_record)
     assert year_new?(new_hash, district, category, year)
   end
 
-  def test_can_tell_if_new_category
+  def test_statewide_parser_can_tell_if_new_category
     new_hash = {"COLORADO" => {:name=>"COLORADO", :third_grade=>{2008=>{:math=>0.697, :reading=>0.703}, 2009=>{:writing=>0.536}}}}
     district_record = {:name=>"COLORADO", :eighth_grade=>{2009=>{:math=>0.499}}}
     district, category, subject, year, data_point = record_data(district_record)
     assert category_new?(new_hash, district, category)
   end
 
-  def test_can_tell_if_not_new_category
+  def test_statewide_parser_can_tell_if_not_new_category
     new_hash = {"COLORADO" => {:name=>"COLORADO", :third_grade=>{2008=>{:math=>0.697, :reading=>0.703}, 2009=>{:writing=>0.536}}}}
     district_record = {:name=>"COLORADO", :third_grade=>{2009=>{:writing=>0.536}}}
     district, category, subject, year, data_point = record_data(district_record)
     refute category_new?(new_hash, district, category)
   end
 
-  def test_record_data_collect_correct_data
+  def test_statewide_parser_record_data_collect_correct_data
     district_record = {:name=>"COLORADO", :eighth_grade=>{2009=>{:math=>0.499}}}
     assert_equal "COLORADO", district_record[:name]
     assert_equal :eighth_grade, district_record.keys[1]
@@ -117,7 +118,7 @@ class CSVParserTest < Minitest::Test
     assert_equal expected, record_data(district_record)
   end
 
-  def test_statewide_repo_parser_only_has_one_hash_per_district
+  def test_statewide_parser_only_has_one_hash_per_district
     file_tree = {
       :statewide_testing => {
       :third_grade => "./test/test_data/test 3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
@@ -130,4 +131,51 @@ class CSVParserTest < Minitest::Test
     assert_equal 18, statewide_test_repo_parser(file_tree).count
   end
 
+  def test_economic_parser_can_parse_lunch_record_data
+    district_record = {:free_or_reduced_price_lunch=>{2012=>{:total=>3006}}, :name=>"ACADEMY 20"}
+    expected = [:free_or_reduced_price_lunch, 2012, {:total => 3006}, :total, "ACADEMY 20"]
+    assert_equal expected, lunch_record_data(district_record)
+  end
+
+  def test_economic_parser_can_parse_other_record_data
+    district_record = {:title_i=>{2012=>0.01072}, :name=>"ACADEMY 20"}
+    expected = [:title_i, 2012, 0.01072, "ACADEMY 20"]
+    assert_equal expected, other_record_data(district_record)
+  end
+
+  def test_economic_parser_can_tell_if_new_district
+    new_hash = {}
+    district_record = {:children_in_poverty=>{1997=>0.035}, :name=>"ACADEMY 20"}
+    category, year, data_point, district = other_record_data(district_record)
+    assert district_new?(new_hash, district)
+  end
+
+  def test_economic_parser_can_tell_if_new_year
+    new_hash = {"ACADEMY 20" =>{:children_in_poverty=>{1997=>0.035}, :name=>"ACADEMY 20"} }
+    district_record = {:children_in_poverty=>{1990=>0.035}, :name=>"ACADEMY 20"}
+    category, year, data_point, district = other_record_data(district_record)
+    assert year_new?(new_hash, district, category, year)
+  end
+
+  def test_economic_parser_can_tell_if_new_category
+    new_hash = {"ACADEMY 20" =>{:children_in_poverty=>{1997=>0.035}, :name=>"ACADEMY 20"} }
+    district_record = {:title_i=>{2012=>0.01072}, :name=>"ACADEMY 20"}
+    category, year, data_point, district = other_record_data(district_record)
+    assert category_new?(new_hash, district, category)
+  end
+
+  def test_can_economic_parser_can_tell_if_not_new_category
+    new_hash = {"ACADEMY 20" =>{:children_in_poverty=>{1997=>0.035}, :name=>"ACADEMY 20"} }
+    district_record = {:children_in_poverty=>{1995=>0.032}, :name=>"ACADEMY 20"}
+    category, year, data_point, district = other_record_data(district_record)
+    refute category_new?(new_hash, district, category)
+  end
+
+  def test_economic_parser_can_tell_if_new_type
+    new_hash = {"ACADEMY 20" => {:free_or_reduced_price_lunch=>{2012=>{:total=>3006}}, :name=>"ACADEMY 20"}}
+    district_record = {:free_or_reduced_price_lunch=>{2012=>{:percentage=>0.0621}}, :name=>"ACADEMY 20"}
+    category, year, data_point, type, district = lunch_record_data(district_record)
+    assert type_new?(new_hash, district, category, year, type)
+  end
 end
+# new_hash = {"COLORADO" => {:children_in_poverty =>{1998=> 0.036}, :name =>"COLORADO"}}
--- test/economic_profile_repo_test.rb
+++ test/economic_profile_repo_test.rb
@@ -8,14 +8,13 @@ require 'rake/testtask'
 class EconomicProfileRepoTest < Minitest::Test
 
   def test_can_load_data
-    skip
     epr = EconomicProfileRepository.new
     epr.load_data({
       :economic_profile => {
-      :median_household_income => "./data/Median household income.csv",
-      :children_in_poverty => "./data/School-aged children in poverty.csv",
-      :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
-      :title_i => "./data/Title I students.csv"}})
+      :median_household_income => "./test/test_data/test Median household income.csv",
+      :children_in_poverty => "./test/test_data/test School-aged children in poverty.csv",
+      :free_or_reduced_price_lunch => "./test/test_data/test Students qualifying for free or reduced price lunch.csv",
+      :title_i => "./test/test_data/test Title I students.csv"}})
     refute epr.profiles.empty?
   end
 
@@ -24,10 +23,10 @@ class EconomicProfileRepoTest < Minitest::Test
     epr = EconomicProfileRepository.new
     epr.load_data({
       :economic_profile => {
-      :median_household_income => "./data/Median household income.csv",
-      :children_in_poverty => "./data/School-aged children in poverty.csv",
-      :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
-      :title_i => "./data/Title I students.csv"}})
+      :median_household_income => "./test/test_data/test Median household income.csv",
+      :children_in_poverty => "./test/test_data/test School-aged children in poverty.csv",
+      :free_or_reduced_price_lunch => "./test/test_data/test Students qualifying for free or reduced price lunch.csv",
+      :title_i => "./test/test_data/test Title I students.csv"}})
     refute epr.economic_profile.empty?
     ep = epr.find_by_name("ACADEMY 20")
     expected =
